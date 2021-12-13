fbd8bfb660cc8be10e3e468577cddde86c52412b
ndmvisuals <70617650+ndmvisuals@users.noreply.github.com>
2021-12-13 14:21:47 -0500

    add

--- police-logs-app/deploy.R
+++ police-logs-app/deploy.R
@@ -7,14 +7,21 @@ error_on_missing_name <- function(name){
   }
   gsub("\"", '',var)
 }
+
+
+
+
 # Authenticate
 setAccountInfo(name = error_on_missing_name("SHINY_ACC_NAME"),
                token = error_on_missing_name("TOKEN"),
                secret = error_on_missing_name("SECRET"))
 # Deploy the application.
 deployApp(
-  appFiles = c("app.R" #, you can specify which files to deploy, 
-               #or keep this NULL to deploy everything
-  ),
+  appDir = setwd("C:/Users/nicho/Documents/GitHub/university-police-logs"), 
+  appFiles = c("police_logs_app.R", "/data/arrest_combined.rds", "/data/umd_arrest.rds", "/data/umd_incident.rds"),
   appName = error_on_missing_name("MASTERNAME"),
   appTitle = "shinyapplication")
+
+
+
+
