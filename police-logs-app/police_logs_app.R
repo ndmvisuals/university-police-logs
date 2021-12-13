e84993268199ae9b96cf32a86d9b539b675305a8
ndmvisuals <70617650+ndmvisuals@users.noreply.github.com>
2021-12-13 09:04:40 -0500

    centered time of day column

--- police-logs-app/police_logs_app.R
+++ police-logs-app/police_logs_app.R
@@ -387,7 +387,7 @@ server <- function(input, output, session){
       theme_ipsum_rc(grid="Y")+
       scale_x_discrete( labels = c("12 a.m.", "1 a.m.", "2 a.m.", "3 a.m.", "4 a.m.", "5 a.m.", "6 a.m.", "7 a.m.","8 a.m.", "9 a.m.", "10 a.m.", "11 a.m.",
                                    "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.", "5 p.m.", "6 p.m.", "7 p.m.","8 p.m.", "9 p.m.", "10 p.m.", "11 p.m."))+
-      theme(legend.position = "none")
+      theme(legend.position = "none", axis.text.x = element_text(angle = 90, vjust=0.5))
     
     
     
